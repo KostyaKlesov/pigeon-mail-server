08fba1b911582cf17ddfec138774b36e7de2ce36
KostyaKlesov <klyosov03@mail.ru>
2024-06-07 14:29:40 +0300

    4 commit

--- client.c
+++ client.c
@@ -4,44 +4,53 @@
 #include <arpa/inet.h>
 #include <stdlib.h>
 
-void sendMessage(char* message){
-    sleep(3600);
-    if (rand() % 2 == 0) {
-        printf("Голубь успешно доставил сообщение: %s\n", message);
-    } else {
-        printf("К сожалению, голубь потерялся по пути.\n");
+char sendMessage(char* code[6]){
+    char buf[100]; 
+    char message[6];
+    //Копируем code в buf
+    //strcpy(buf, code);
+    // разбиваем код на части по 5 символов
+    for (int i = 0; i < sizeof(code) - 2; i++) {
+        printf("Вошли в цикл %d\n", i);
+        strncpy(message, code, 5);
+        //printf("%s\n", message);
+        message[5] = '\0';
+        if (rand() % 2 == 0){
+            strcpy(buf, message);
+            return buf;
+        } else{
+            continue;
+        }
+        memset(&message[0], 0, sizeof(message));
     }
+    //strcpy(buf, message);
+    //sleep(3600);
 }
 
 
 int main(void)
 {
-   
+    char code[6];
     int s;
+    char buf;
     unsigned short port;
     struct sockaddr_in server;
-    char buf[5]; 
-    char code[6];
-    //char message[5];
-    char message[6];
+    // проверка на создание сокета 
     if ((s = socket(AF_INET, SOCK_DGRAM, 0)) < 0)
     {
         perror("socket()");
         exit(1);
     }
+    // выбор протокала, порта и адроеса
     server.sin_family = AF_INET;
     server.sin_port = ntohs(3590);
     server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    fgets(code, sizeof(code), stdin);
-    code[strcspn(code, "\n")] = 0; 
-    strcpy(buf, code);
-    for (int i = 0; i < strlen(code); i += 5) {
-        strncpy(message, code + i, 5);
-        message[5] = '\0';
-        sendOverPigeon(message);
-    }
-    strcpy(buf, message);
-    printf("Введенное сообщение: %s\n", &buf);
+    // ввод строки кода
+    //code[strcspn(code, "\n")] = 0; 
+    fgets(code, sizeof(code)+1, stdin);
+    //printf("Введенное сообщение: %s\n", &buf);
+    buf = sendMessage(code);
+    // отправка сообщения
     if (sendto(s, buf, (strlen(buf)+1), 0,(struct sockaddr *)&server, sizeof(server)) < 0)
     {
         perror("sendto()");
